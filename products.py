6cae739ef3176f05d37f71634377398710f6f2ec
GabiMorales <gabrimorales381@gmail.com>
2019-09-04 14:49:07 -0400

    Add files via upload

---
+++ products.py
@@ -0,0 +1,6 @@
+products = [
+        {"name":"keyboard", "price": 80000, "quantity": 4},
+        {"name":"monitor", "price": 20000, "quantity": 1},
+        {"name":"mouse", "price": 30000, "quantity": 2},
+        {"name":"laptop", "price": 60000, "quantity": 3}
+]
